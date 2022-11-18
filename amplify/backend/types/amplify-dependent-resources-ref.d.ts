a87bdadaf1bdbca73ebd4b12edab7285e750670e
Long Huynh <long.l.huynh@gmail.com>
2022-11-18 09:55:45 -0500

    broken version

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,27 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "imagesegmentation618d25f7": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "function": {
+        "S3Trigger98e07da5": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "storage": {
+        "s3de8b8aba": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,18 @@
-function Login() {
+import { Amplify } from 'aws-amplify';
+
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+function Login({ signOut, user }) {
   return (
-    <div className="Login">
-      <h1>Login Page</h1>
-    </div>
+    <>
+    <h1>Hello {user.username}</h1>
+    <button onClick={signOut}>Sign out</button>
+    </>
   );
 }
 
-export default Login;
+export default withAuthenticator(Login);
